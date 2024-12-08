251b3f52adaa5a9878718d7207d1b3422b9272ec
LizaZaharenko363 <liza.zaharenko25@gmail.com>
2024-12-08 17:30:37 +0200

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component1Component } from './component1/component1.component';
+import { Component3Component } from './component3/component3.component';
+import { Component4Component } from './component4/component4.component';
+import { Component5Component } from './component5/component5.component';;
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    Component1Component,
+    Component3Component,
+    Component4Component,
+    Component5Component
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'lab8';
